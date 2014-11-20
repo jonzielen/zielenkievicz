3bf130c92338fdf9b959834951514be603493e80
jonzielen <jonzielen@gmail.com>
2014-11-20 14:25:58 -0500

    add modal to site

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-
+$(function() {
+  $('.slideshow').each(function() {
+    var slideShowBox = '<div class="slideshowbox"><h3>Slideshow Available</h3></div>';
+    $(this).append(slideShowBox);
+  });
+});
--- public/js/plugins.js
+++ public/js/plugins.js
@@ -22,3 +22,73 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+// MODAL PLUGIN
+(function($) {
+  // basic modal
+  var modalConent = '<div id="modal"></div><div id="modal-holder"><div id="modal-close">X</div><div id="modal-content-holder"></div></div>';
+
+  // content to add to modal
+  var conentToAdd = $('#modal-content').clone();
+  $('#modal-content').remove();
+
+  // centers modal
+  $.fn.center =  function() {
+    var windowWidth = $(window).width()/2; // screen width
+    var videoWidth  = $(this).outerWidth()/2; // video width
+    var finalWidth  = windowWidth-videoWidth; // subtract to get the center
+
+    if ((windowWidth-videoWidth) > 0) {
+      $('#modal-holder').css({
+        'left':finalWidth,
+        'margin-right':finalWidth
+      });
+    } else {
+      $('#modal-holder').css({
+        'left':0,
+        'margin-right':0
+      });
+
+      $('#modal-close').css({
+        'right':0,
+        'top':(-55)+'px'
+      });
+    };
+  };
+
+  //remove modal
+  $.fn.removeModal = function() {
+    $('#modal, #modal-holder').fadeOut(300, function() {
+      $('#modal, #modal-holder').remove();
+    });
+  };
+
+  // add modal to body
+  $.fn.modal = function() {
+    $('body').append(modalConent);
+    $('#modal-content-holder').append(conentToAdd);
+    $('#modal-holder').center();
+  };
+
+  // removes modal
+  $('body').on('click', '#modal, #modal-close', function() {
+    $(this).removeModal();
+  });
+
+  // launch modal
+  $('.launchModal').on('click', function(e) {
+    $(this).modal();
+    e.preventDefault();
+  });
+
+  // esc key removes modal
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      $(this).removeModal();
+    };
+    e.preventDefault();
+  });
+
+  $(window).resize(function() {
+    $('#modal-holder').center();
+  });
+}(jQuery));
