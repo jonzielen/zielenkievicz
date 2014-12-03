df7e9505083043f378ba5f969939e5f1c70ffb27
jonzielen <jonzielen@gmail.com>
2014-12-03 16:51:48 -0500

    refactor main js code

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,4 @@
 $(function() {
-  // add slideshow box
-  /*$('.slide').each(function() {
-    var slideShowBox = '<div class="slideshowbox"></div>';
-    $(this).append(slideShowBox);
-  });*/
-
   $('.notMmaintained').each(function() {
     var message = 'Aspects of this code have changed since<br />I last worked on this.';
     var container = '<div class="slideshowbox"><h3>'+message+'</h3></div>';
@@ -12,9 +6,4 @@ $(function() {
     $(this).addClass('slide');
     $(this).append(container);
   });
-
-
-
-
-
 });
